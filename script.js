ddbe9cc99465a964ca98b7f71003745dc82b3976
Manish9699 <manishmahadadalkar18@gmail.com>
2023-08-11 18:07:00 +0530

    Implementing logic

---
+++ script.js
@@ -0,0 +1,41 @@
+function getComputerChoice() {
+  let random = Math.floor(Math.random() * 3);
+  if (random === 0) {
+    const cInput = "ROCK";
+    return cInput;
+  } else if (random === 1) {
+    const cInput = "PAPER";
+    return cInput;
+  } else {
+    const cInput = "SCISSORS";
+    return cInput;
+  }
+}
+
+function getUserChoice() {
+  let input = prompt("Enter what you want to choose");
+  return input.toUpperCase();
+}
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === "ROCK" && computerSelection === "PAPER") {
+    return "Computer wins";
+  } else if (computerSelection === "ROCK" && playerSelection === "PAPER") {
+    return "Player wins";
+  } else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
+    return "Computer wins";
+  } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
+    return "Computer wins";
+  } else if (computerSelection === "PAPER" && playerSelection === "SCISSORS") {
+    return "Player wins";
+  } else if (computerSelection === "SCISSORS" && playerSelection === "ROCK") {
+    return "Player wins";
+  } else {
+    return "It's a tie";
+  }
+}
+
+const playerSelection = getUserChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
